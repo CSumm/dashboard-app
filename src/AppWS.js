41109b32280183c6d750ca930dd83717f85b4f9e
Carl Summers <31711543+CSumm@users.noreply.github.com>
2021-06-26 00:20:31 -0400

    Update AppWS.js

--- src/AppWS.js
+++ src/AppWS.js
@@ -1,12 +1,18 @@
-import {useRef, useEffect, useContext} from 'react';
+import {useRef, useEffect, useContext, useState} from 'react';
 import { DataContext } from './DataContext';
+import {useInterval} from './useInterval';
 
 export default function AppWs() {
     const ws = useRef(null);
     const {setWarning, setLiveData, isSocketOpen} = useContext(DataContext);
+    const [isRunning,setIsRunning] = useState(true);
+
+    useInterval(()=> {
+        ws.current.send('');
+    }, isRunning ? 20000 : null);
    
     useEffect(() => {
-        ws.current = new WebSocket("ws://localhost:3001");
+        ws.current = new WebSocket("wss://dashboard-app-backend.herokuapp.com/");
         ws.current.onopen = () => console.log("ws opened");
         ws.current.onclose = () => console.log("ws closed");
         ws.current.onmessage = evt => {
@@ -17,6 +23,7 @@ export default function AppWs() {
 
         if(isSocketOpen === false){
             ws.current.close();
+            setIsRunning(false);
         }
 
         return () => {
